fb17848e0560151b737a1eb306ce173f35a88319
sprout2000 <wassabie64@gmail.com>
2022-02-13 17:52:29 +0900

    feat: Updated front end.

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 import './styles.css';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  const onCountUp = () => setCount((count) => count + 1);
+  const onCountDown = () => setCount((count) => count - 1);
+
   return (
     <div className="container">
       <h1>Hello.</h1>
+      <h2>{count}</h2>
+      <div>
+        <button onClick={onCountDown}>Down</button>
+        <button onClick={onCountUp}>Up</button>
+      </div>
     </div>
   );
 };
