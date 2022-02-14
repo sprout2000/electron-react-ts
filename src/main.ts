503ffd5661977797de5abd6088b8b079b3ae31b1
sprout2000 <wassabie64@gmail.com>
2022-02-14 13:28:40 +0900

    perf: Simplified.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import path from 'path';
-import { BrowserWindow, app, session, nativeTheme } from 'electron';
+import { BrowserWindow, app, session } from 'electron';
 import { searchDevtools } from 'electron-search-devtools';
 
 const isDev = process.env.NODE_ENV === 'development';
@@ -20,8 +20,6 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 600,
     height: 400,
-    show: false,
-    backgroundColor: nativeTheme.shouldUseDarkColors ? '#1e1e1e' : '#f6f6f6',
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
@@ -29,8 +27,6 @@ const createWindow = () => {
 
   if (isDev) mainWindow.webContents.openDevTools({ mode: 'detach' });
   mainWindow.loadFile('dist/index.html');
-
-  mainWindow.once('ready-to-show', () => mainWindow.show());
 };
 
 app.whenReady().then(async () => {
@@ -44,15 +40,6 @@ app.whenReady().then(async () => {
   }
 
   createWindow();
-});
-
-app.setAboutPanelOptions({
-  applicationName: app.name,
-  applicationVersion:
-    process.platform === 'darwin'
-      ? app.getVersion()
-      : `v${app.getVersion()} (${process.versions['electron']})`,
-  version: process.versions['electron'],
 });
 
 app.once('window-all-closed', () => app.quit());
