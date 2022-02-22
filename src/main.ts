77e2aa2287650d3fa19a4c660a05a01b7430e793
sprout2000 <wassabie64@gmail.com>
2022-02-22 14:30:03 +0900

    chore: Removed search-devtools.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import path from 'path';
-import { BrowserWindow, app, ipcMain, session } from 'electron';
-import { searchDevtools } from 'electron-search-devtools';
+import { BrowserWindow, app, ipcMain } from 'electron';
 
 const isDev = process.env.NODE_ENV === 'development';
 
@@ -10,7 +9,6 @@ if (isDev) {
       ? '../node_modules/electron/dist/electron.exe'
       : '../node_modules/.bin/electron';
 
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
   require('electron-reload')(__dirname, {
     electron: path.resolve(__dirname, execPath),
   });
@@ -27,21 +25,8 @@ const createWindow = () => {
     mainWindow.setTitle(`Electron React TypeScript: ${arg}`);
   });
 
-  if (isDev) mainWindow.webContents.openDevTools({ mode: 'detach' });
   mainWindow.loadFile('dist/index.html');
 };
 
-app.whenReady().then(async () => {
-  if (isDev) {
-    const devtool = await searchDevtools('REACT', { browser: 'google-chrome' });
-    if (devtool) {
-      await session.defaultSession.loadExtension(devtool, {
-        allowFileAccess: true,
-      });
-    }
-  }
-
-  createWindow();
-});
-
+app.whenReady().then(createWindow);
 app.once('window-all-closed', () => app.quit());
