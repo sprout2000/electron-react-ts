237a559e1b2172c2f2b3156ac3599abeba1b790e
sprout2000 <wassabie64@gmail.com>
2022-04-15 11:01:01 +0900

    refactor: Updated ReactDOM.createRoot().

--- src/web/index.tsx
+++ src/web/index.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 
 import { App } from './App';
 import './index.css';
 
-const container = document.getElementById('root');
-const root = container && createRoot(container);
-
-root?.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
