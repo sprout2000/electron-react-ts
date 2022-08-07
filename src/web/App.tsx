06efc7a440be9c665207dc2b0a11c00ca66884fa
sprout2000 <wassabie64@gmail.com>
2022-08-07 14:58:12 +0900

    refactor: Removed a callback fn.

--- src/web/App.tsx
+++ src/web/App.tsx
@@ -5,12 +5,10 @@ import './App.css';
 const App = () => {
   const [count, setCount] = useState(0);
 
-  const onCountUp = () => setCount((count) => count + 1);
-
   return (
     <div className="container">
       <h1>{count}</h1>
-      <button onClick={onCountUp}>Count</button>
+      <button onClick={() => setCount((count) => count + 1)}>Count</button>
     </div>
   );
 };
