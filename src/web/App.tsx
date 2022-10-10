8c37ca0151aa5516193a1150824e897fdfb72f30
sprout2000 <wassabie64@gmail.com>
2022-10-10 10:10:14 +0900

    chore: Updated 'src/web' folder structure.

---
+++ src/web/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import './App.css';
+
+export const App = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={() => setCount((count) => count + 1)}>Count</button>
+    </div>
+  );
+};
--- src/web/index.tsx
+++ src/web/index.tsx
@@ -1,18 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import './index.css';
-
-const App = () => {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="container">
-      <h1>{count}</h1>
-      <button onClick={() => setCount((count) => count + 1)}>Count</button>
-    </div>
-  );
-};
+import { App } from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
