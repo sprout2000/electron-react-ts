0d034adf3184ab1ebf8df0e98b4c18bf44459955
sprout2000 <wassabie64@gmail.com>
2022-11-06 16:11:36 +0900

    refactor: Refactor renderer process.

--- src/web/App.tsx
+++ src/web/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 export const App = () => {
--- src/web/index.tsx
+++ src/web/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 
 import { App } from './App';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root') as Element).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
