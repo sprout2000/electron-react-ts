487d1d2ee32c2ce86d21b220fe8b132424bd06ab
sprout2000 <wassabie64@gmail.com>
2022-11-06 16:14:20 +0900

    chore: Removed electron-nice-auto-reload & electron-builder.

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,28 @@
-import path from 'path';
+import path from 'node:path';
 import { BrowserWindow, app } from 'electron';
 
-if (process.env.NODE_ENV === 'development') {
-  require('electron-nice-auto-reload')({
-    rootPath: path.join(process.cwd(), 'dist'),
-    rules: [{ action: 'app.relaunch' }],
+import { reloader } from './reloader';
+
+const isDev = process.env.NODE_ENV === 'development';
+
+if (isDev) {
+  reloader({
+    mainPaths: ['dist/main.js', 'dist/preload.js'],
+    rendererPaths: ['dist/index.html', 'dist/app.js', 'dist/app.css'],
   });
 }
 
 app.whenReady().then(() => {
-  new BrowserWindow().loadFile('dist/index.html');
+  const mainWindow = new BrowserWindow({
+    show: false,
+    webPreferences: { preload: path.resolve(__dirname, 'preload.js') },
+  });
+
+  mainWindow.loadFile('dist/index.html');
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+    // if (isDev) mainWindow.webContents.openDevTools({ mode: 'detach' });
+  });
 });
 
 app.once('window-all-closed', () => app.quit());
