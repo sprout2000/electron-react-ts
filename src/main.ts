849f3cc07670c2b53bce84842233d21d3ea50e31
sprout2000 <wassabie64@gmail.com>
2022-11-16 13:06:06 +0900

    feat: Use electronmon instead of chokidar.

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,15 @@
 import path from 'node:path';
 import { BrowserWindow, app } from 'electron';
 
-import { reloader } from './reloader';
-
-const isDev = process.env.NODE_ENV === 'development';
-
-if (isDev) {
-  reloader({
-    mainPaths: ['dist/main.js', 'dist/preload.js'],
-    rendererPaths: ['dist/index.html', 'dist/app.js', 'dist/app.css'],
-  });
-}
-
 app.whenReady().then(() => {
   const mainWindow = new BrowserWindow({
-    show: false,
-    webPreferences: { preload: path.resolve(__dirname, 'preload.js') },
+    webPreferences: {
+      preload: path.resolve(__dirname, 'preload.js'),
+    },
   });
 
   mainWindow.loadFile('dist/index.html');
-  mainWindow.once('ready-to-show', () => {
-    mainWindow.show();
-    // if (isDev) mainWindow.webContents.openDevTools({ mode: 'detach' });
-  });
+  // mainWindow.webContents.openDevTools({ mode: 'detach' });
 });
 
 app.once('window-all-closed', () => app.quit());
